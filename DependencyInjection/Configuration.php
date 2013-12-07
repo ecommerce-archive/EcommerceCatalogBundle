a978bc8aeaf56e5bd4077562aa7da504214d8301
Philipp Wahala <philipp.wahala@gmail.com>
2013-12-07 22:13:23 +0100

    Initial commit - Bundle structure

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ecommerce\Bundle\CatalogBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Philipp Wahala <philipp.wahala@gmail.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree.
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ecommerce_catalog');
+
+
+        return $treeBuilder;
+    }
+}
---
+++ EcommerceCatalogBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ecommerce\Bundle\CatalogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Philipp Wahala <philipp.wahala@gmail.com>
+ */
+class EcommerceCatalogBundle extends Bundle
+{
+}
