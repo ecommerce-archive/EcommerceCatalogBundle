a879f51f0d1f0d2c90b347002e469244a238c354
Philipp Wahala <philipp.wahala@gmail.com>
2013-12-07 22:48:47 +0100

    Rename configs

--- DependencyInjection/EcommerceCatalogExtension.php
+++ DependencyInjection/EcommerceCatalogExtension.php
@@ -21,6 +21,6 @@ class EcommerceCatalogExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader->load('controllers.xml');
     }
 }
